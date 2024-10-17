86ccd61b6939937a7e9a9a5b2b1a7a53bb63844d
chani999 <psc0729@naver.com>
2024-10-17 17:18:56 +0900

    jpql 연습

---
+++ src/main/java/com/study/jpa/chap02/entity/Student.java
@@ -0,0 +1,29 @@
+package com.study.jpa.chap01.chap02.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Setter @Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Builder
+
+@Entity
+@Table(name = "tbl_student")
+public class Student {
+
+    @Id
+    @Column(name = "stu_id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(name = "stu_name", nullable = false)
+    private String name;
+
+    private String city;
+
+    private String major;
+
+}
---
+++ src/main/java/com/study/jpa/chap02/repository/StudentRepository.java
@@ -0,0 +1,42 @@
+package com.study.jpa.chap01.chap02.repository;
+
+
+import com.study.jpa.chap01.chap02.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface StudentRepository extends JpaRepository<Student, String> {
+
+    List<Student> findByName(String name);
+
+    List<Student> findByCityAndMajor(String city, String major);
+
+    // major 로 찾는다, %major% 와같은
+    List<Student> findByMajorContaining(String major);
+
+    // select City where age >= ?
+    List<Student> findCityByAgeGreaterThanEqual(int age);
+
+
+    // native-sql
+    // SELECT 컬럼명 FROM 테이블명
+    // WHERE 컬럼 = ?
+
+    // JPQL
+    // SELECT 별칭 FROM 엔터티클래스명 AS 별칭
+    // WHERE 별칭.필드명 = ?
+    List<Student> findByNameOrCity(String name, String city);
+
+    @Query("select st from Student st where st.name like %?1% ")
+    List<Student> serachByNameWithJPQL(String name);
+
+    @Query("select st from Student st where st.city = ?1 ")
+    Optional<Student> getByCityWithJPQL(String city);
+
+    @Query("delete from Student s where s.name = ?1 and s.city = ?2 ")
+    void deleteByNameAndCityWithJPQL(String name,String city);
+}

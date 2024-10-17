463b7df434f44afad4011334c67912f65558c688
chani999 <psc0729@naver.com>
2024-10-17 17:46:13 +0900

    jpql 다대일 조인 연습

---
+++ src/main/java/com/study/jpa/chap04/entity/Department.java
@@ -0,0 +1,24 @@
+package com.study.jpa.chap04.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Builder
+
+@Entity
+@Table(name = "tbl_student")
+public class Deparment {
+    @Id
+    @Column(name = "dept_id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(name = "dept_name", nullable = false)
+    private String name;
+}
